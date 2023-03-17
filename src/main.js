7a826c9af047a833b2b74a6753426fd75b8d7d20
sekaiiiii <qq738359456@gmail.com>
2023-03-17 15:43:34 +0800

    feat: combind vue & electron project

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// main.js
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  mainWindow.loadURL('http://localhost:8080')
+  // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
