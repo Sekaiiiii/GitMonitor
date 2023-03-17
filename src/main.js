3cdf6f4b1f41535c09477d038b5094852711e086
sekaiiiii <qq738359456@gmail.com>
2023-03-17 17:28:43 +0800

    feat: the demo for vue & electron communication

--- src/main.js
+++ src/main.js
@@ -1,26 +1,54 @@
 // main.js
 
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const moment = require('moment');
 
-const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+const vueClientWindowApi = {
+
+}
+
+const createVueClientWindow = () => {
+  const vueClientWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, 'lib', 'vue-client-preload.js')
+    }
   })
 
-  mainWindow.loadURL('http://localhost:8080')
-  // mainWindow.webContents.openDevTools()
+  ipcMain.handle('invoke', (event, message) => {
+    console.log('INVOKE', message);
+    return message;
+    // todo: dispatch event
+  })
+  ipcMain.on('sendResponse', (event, message) => {
+    console.log('sendResponse', message);
+  })
+
+  // todo: pack funciton to send message to renderer
+  vueClientWindowApi.send = (message) => {
+    vueClientWindow.webContents.send('handler', message)
+  }
+
+  vueClientWindow.loadURL('http://localhost:8080')
+  vueClientWindow.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
-  createWindow()
-
+  createVueClientWindow()
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createVueClientWindow()
   })
+  // test
+  setInterval(() => {
+    vueClientWindowApi.send(moment().format('YYYY-MM-DD HH:mm:ss'));
+  }, 1000)
+
 })
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
+
